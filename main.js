84bc52f4e7f579892e68a06a9b0d6f73f0c6ade8
PavelVoykov <71674555+PavelVoykov@users.noreply.github.com>
2021-02-27 02:22:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ Game.load.spritesheet("ball", "ball.512x512.4x4.png", 512/4, 512/4)
 Game.load.image('bg', "space2.png")
 Game.load.audio("Space", "INFINITY - Epic Futuristic Music Mix Atmospheric Sci-Fi Music.mp3")
 Game.load.audio("Hit", "HIT.mp3");
+Game.load.audio("Miss", "Miss.mp3");
+Game.load.audio("WIN", "WIN.mp3");
 }
 let platform
 let platform2
@@ -21,13 +23,22 @@ let textWIN1
 let platformwidth = 60
 let num = 0.5
 let bgmusic
+let hitsound
+let scoresound
+let WIN
 function create() {
     bgmusic = Game.add.audio("Space")
     bgmusic.volume = 0.1
     //console.log(bgmusic.volume)
-    bgmusic.play();
+    bgmusic.play("",[], 0.1, true);
 
     hitsound = Game.add.audio("Hit");
+    hitsound.volume = 0.1
+    console.log(hitsound.volume)
+    scoresound = Game.add.audio("Miss");
+    scoresound.volume = 0.1
+    WIN = Game.add.audio("WIN");
+    WIN.volume = 0.1
     
 
     bg = Game.add.sprite(0, 0, 'bg')
@@ -79,6 +90,7 @@ function create() {
 }
 let count = 1
 let dead = 1
+let count0 = 0
 function update(){
     
     Game.physics.arcade.collide(ball, platform, onHit);
@@ -90,8 +102,10 @@ function update(){
     textS0.text = "Beige Team\nScore: " + score1
     if(ball.x < 512/8+1 || ball0.x < 512/8+1){
         SCORERED();
+        scoresound.play();
     }else if(ball.x > Game.width - 512/8-1 || ball0.x > Game.width - 512/8-1){
         SCOREBAGE();
+        scoresound.play();
     }
     if(!(count%400) && dead == 1){
         num = Game.rnd.integerInRange(0, 1)
@@ -136,52 +150,47 @@ function update(){
     }else if(!(Game.input.keyboard.isDown(Phaser.KeyCode.UP))){
         platform2.animations.stop()
     }
+    
    if(score0==10){
     textWIN0 = Game.add.text(Game.width/2, Game.height/2,"Red Team\n    WINS",{font:"65px Arial",fill:"rgb(220,20,60)"})
-        textWIN0.anchor.setTo(0.5)
-        ball.x = Game.width/2
-        ball.y = Game.height/2
-        ball0.x = Game.width/2
-        ball0.y = Game.height/2
-        platform.y = Game.height/2
-        platform2.y = Game.height/2
-        ball.body.velocity.x = 0
-        ball.body.velocity.y = 0
-        ball0.body.velocity.x = 0
-        ball0.body.velocity.y = 0
+    textWIN0.anchor.setTo(0.5) 
+    OnWin(count0);
+    count0++
    }
    if(score1==10){
-    textWIN1 = Game.add.text(Game.width/2, Game.height/2,"Beige Team\n    WINS",{font:"65px Arial",fill:"rgb(225,198,153)"})
-        textWIN1.anchor.setTo(0.5) 
-        ball.x = Game.width/2
-        ball.y = Game.height/2
-        ball0.x = Game.width/2
-        ball0.y = Game.height/2
-        platform.y = Game.height/2
-        platform2.y = Game.height/2
-        ball.body.velocity.x = 0
-        ball.body.velocity.y = 0
-        ball0.body.velocity.x = 0
-        ball0.body.velocity.y = 0
+    textWIN1 = Game.add.text(Game.width/2, Game.height/2,"Beige Team\n    WINS",{font:"65px Arial",fill:"#ffdd99"})
+    textWIN1.anchor.setTo(0.5) 
+    OnWin(count0);
+    count0++
+    
    }
-   bgmusic.volume = 0.1
 
+}
+function OnWin(m){
+    ball.x = Game.width/2
+    ball.y = Game.height/2
+    ball0.x = Game.width/2
+    ball0.y = Game.height/2
+    platform.y = Game.height/2
+    platform2.y = Game.height/2
+    platform.body.gravity.set(0, 0);
+    platform2.body.gravity.set(0, 0);
+    ball.body.velocity.x = 0
+    ball.body.velocity.y = 0
+    ball0.body.velocity.x = 0
+    ball0.body.velocity.y = 0
+    if(m == 0){
+        hitsound.stop();
+        bgmusic.stop();
+        WIN.play();
+    }
 }
 function onHit(){
     ball.body.velocity.y = Game.rnd.integerInRange(-500, 500);
     ball0.body.velocity.y = Game.rnd.integerInRange(-500, 500);
     hitsound.play();
 }
-function start() {
 
-    bgmusic.fadeIn(0);
-    console.log("1");
-    
-}
-function start2() {
-    bgmusic.fadeIn(0);
-    console.log("1");
-}
 function SCOREBAGE(){
     score1++
     ball.x = Game.width/2
